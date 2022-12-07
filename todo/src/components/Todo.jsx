c7ba2b610ce1860efe3a41ac0e4ac8443069edd3
sahid jamal <sahidjamal151@gmail.com>
2022-12-07 16:39:06 +0530

    input created

---
+++ todo/src/components/Todo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Todo = ()=>{
+    return (
+        <>
+    <h1>Todo App</h1>
+    <input type="text" placeholder="Enter Todo" />
+    <button>Add</button>
+    
+    </>
+    )
+}
+export default Todo;
